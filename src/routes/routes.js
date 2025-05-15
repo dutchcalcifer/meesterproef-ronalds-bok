79a33a06eac336cae770d66b96d210dc93618343
Dante Piekart <dantepiekart@gmail.com>
2025-05-15 10:53:11 +0200

    Route error handling refactor

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,7 @@ import { fetchApiData } from "../controllers/api-controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const query = req.query.q || "";
     const results = await getSearchResults(query);
@@ -18,16 +18,11 @@ router.get("/", async (req, res) => {
       results,
     });
   } catch (error) {
-    console.error("Search Error:", error);
-    res.status(500).render("pages/errors/500.ejs", {
-      layout: "layout/layout",
-      title: "500",
-      className: "500",
-    });
+    next(error);
   }
 });
 
-router.get("/item/:id", async (req, res) => {
+router.get("/item/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const item = await getItemDetail(id);
@@ -39,18 +34,16 @@ router.get("/item/:id", async (req, res) => {
       item,
     });
   } catch (error) {
-    console.error("Error fetching item details:", error);
-    res.status(500).send("Internal Server Error");
+    next(error);
   }
 });
 
-router.get("/index", async (req, res) => {
+router.get("/index", async (req, res, next) => {
   try {
     const data = await fetchApiData();
     res.json(data);
   } catch (error) {
-    console.error("Failed to fetch API data:", error);
-    res.status(500).json({ error: "Failed to fetch API data" });
+    next(error);
   }
 });
 
