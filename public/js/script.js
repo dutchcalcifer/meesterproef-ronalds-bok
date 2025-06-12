668c5516c182bc312441477910ed44ec06b8a494
lorenzohorden <122381790+lorenzohorden@users.noreply.github.com>
2025-06-12 14:37:20 +0200

    opslaan op details pagina toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -98,70 +98,93 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
- 
-function getSavedIds() {
-  const cookie = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('savedIds='));
-  if (!cookie) return [];
- 
-  try {
-    return JSON.parse(decodeURIComponent(cookie.split('=')[1]));
-  } catch {
-    return [];
-  }
-}
-
-function setSavedIds(ids) {
-  const days = 30;
-  const date = new Date();
-  date.setTime(date.getTime() + (days*24*60*60*1000));
-  const expires = "expires=" + date.toUTCString();
- 
-  document.cookie = `savedIds=${encodeURIComponent(JSON.stringify(ids))}; ${expires}; path=/`;
-}
 
 // Bookmark toggle
 export function toggleBookmarks () {
-  const buttons = document.querySelectorAll(".results li button");
-  if (buttons.length) {
-    buttons.forEach((button) => {
+  const savedIds = getSavedIds();
+
+  if (window.location.pathname.startsWith("/item")) {
+    const button = document.querySelector("#detailsSaveButton")
+    const pathParts = window.location.pathname.split("/");
+    const id = pathParts[2];
+
+    setEventListener(id, button)
+    loadSavedState(id, button)
+  } else {
+    const buttons = document.querySelectorAll(".results li button");
+
+    buttons.forEach(button => {
       const id = button.parentElement.dataset.id
 
-      const parentLi = button.closest("li");
-      const link = parentLi.querySelector("a[href^='/item/']");
-      if (!link) return;
-
-      button.addEventListener("click", () => {
-        const current = getSavedIds().map(String);
-        const index = current.indexOf(id).toString();
-
-        if (index > -1) {
-          console.log("zit erin")
-          current.splice(index, 1);
-          button.classList.remove("filled");
-        } else {
-          console.log("zit niet erin")
-          current.push(String(id));
-          button.classList.add("filled");
-        }
-
-        setSavedIds(current);
-        
-        if (window.location.pathname === "/saved") {
-          location.reload();
-        }
-
-      })
-    });
+      setEventListener(id, button)
+      loadSavedState(id, button)
+    })
   }
 
-  // Load saved bookmarks
-  const savedIds = getSavedIds();
-  buttons.forEach((button) => {
-    const id = button.parentElement.dataset.id;
+  // if (buttons.length) {
+  //   buttons.forEach((button) => {
+  //     const id = button.parentElement.dataset.id
+
+  //     const parentLi = button.closest("li");
+  //     const link = parentLi.querySelector("a[href^='/item/']");
+  //     if (!link) return;
+
+      
+  //   });
+
+
+
+  // Setting eventlisteners on button
+  function setEventListener (id, button) {
+    button.addEventListener("click", () => {
+      const current = getSavedIds().map(String);
+      const index = current.indexOf(id).toString();
+
+      if (index > -1) {
+        console.log("zit erin")
+        current.splice(index, 1);
+        button.classList.remove("filled");
+      } else {
+        console.log("zit niet erin")
+        current.push(String(id));
+        button.classList.add("filled");
+      }
+
+      setSavedIds(current);
+
+      if (window.location.pathname === "/saved") {
+        location.reload();
+      }
+    })
+  }
+
+  // loading savedstate
+  function loadSavedState(id, button) {
     if (savedIds.includes(id)) {
       button.classList.add("filled");
     }
-  });
+  }
+
+
+  function getSavedIds() {
+    const cookie = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('savedIds='));
+    if (!cookie) return [];
+   
+    try {
+      return JSON.parse(decodeURIComponent(cookie.split('=')[1]));
+    } catch {
+      return [];
+    }
+  }
+
+  function setSavedIds(ids) {
+    const days = 30;
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    const expires = "expires=" + date.toUTCString();
+   
+    document.cookie = `savedIds=${encodeURIComponent(JSON.stringify(ids))}; ${expires}; path=/`;
+  }
 }
